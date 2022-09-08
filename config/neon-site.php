c31fb3768685cfd70a16d322967b9a4763f2153a
Elementary Interactive\Balázs Ercsey <laze@laze.hu>
2022-09-08 16:20:17 +0200

    Hello World Again...
    Some files... This will work once in the future.

---
+++ config/neon-site.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    env('SITE_ID') => [
+        'domain'    => '*'
+    ]
+];
---
+++ src/NeonSiteServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Neon\Site;
+
+use \Illuminate\Support\ServiceProvider;
+use \Illuminate\Contracts\Http\Kernel;
+
+class NeonSiteServiceProvider extends ServiceProvider
+{
+
+  /** Bootstrap any application services.
+   *
+   * @param \Illuminate\Contracts\Http\Kernel  $kernel
+   *
+   * @return void
+   */
+  public function boot(Kernel $kernel): void
+  {
+    if ($this->app->runningInConsole()) {
+      
+      /** Export migrations.
+       */
+      // $this->publishes([
+      //   __DIR__ . '/../database/migrations/create_attributes_table.php.stub'        => database_path('migrations/' . date('Y_m_d_', time()) . '000001_create_attributes_table.php'),
+      //   __DIR__ . '/../database/migrations/create_attribute_values_table.php.stub'  => database_path('migrations/' . date('Y_m_d_', time()) . '000002_create_attribute_values_table.php'),
+      // ], 'neon-migrations');
+    }
+  }
+
+  public function register()
+  {
+    $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'neon');
+  }
+}
