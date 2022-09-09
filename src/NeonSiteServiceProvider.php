536a80ccf67fb1f75277c25f3345fe0ebf61c498
Elementary Interactive\Balázs Ercsey <laze@laze.hu>
2022-09-09 14:53:37 +0200

    Publish config.

--- src/NeonSiteServiceProvider.php
+++ src/NeonSiteServiceProvider.php
@@ -14,21 +14,32 @@ class NeonSiteServiceProvider extends ServiceProvider
    *
    * @return void
    */
-  public function boot(Kernel $kernel): void
+  // public function boot(Kernel $kernel): void
+  // {
+  //   if ($this->app->runningInConsole()) {
+      
+  //     /** Export migrations.
+  //      */
+  //     // $this->publishes([
+  //     //   __DIR__ . '/../database/migrations/create_attributes_table.php.stub'        => database_path('migrations/' . date('Y_m_d_', time()) . '000001_create_attributes_table.php'),
+  //     //   __DIR__ . '/../database/migrations/create_attribute_values_table.php.stub'  => database_path('migrations/' . date('Y_m_d_', time()) . '000002_create_attribute_values_table.php'),
+  //     // ], 'neon-migrations');
+  //   }
+  // }
+
+  public function boot()
   {
     if ($this->app->runningInConsole()) {
-      
-      /** Export migrations.
-       */
-      // $this->publishes([
-      //   __DIR__ . '/../database/migrations/create_attributes_table.php.stub'        => database_path('migrations/' . date('Y_m_d_', time()) . '000001_create_attributes_table.php'),
-      //   __DIR__ . '/../database/migrations/create_attribute_values_table.php.stub'  => database_path('migrations/' . date('Y_m_d_', time()) . '000002_create_attribute_values_table.php'),
-      // ], 'neon-migrations');
+
+      $this->publishes([
+        __DIR__.'/../config/config.php'   => config_path('neon-config.php'),
+      ], 'neon-site-config');
+
     }
   }
 
-  public function register()
-  {
-    $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'neon-config');
-  }
+  // public function register()
+  // {
+  //   $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'neon-config');
+  // }
 }
