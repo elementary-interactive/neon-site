cf59fec5661f3f2fd53d8262fc5bc8813eceafad
Balazs Ercsey <balazs.ercsey@elementary-interactive.com>
2022-11-14 17:09:01 +0100

    Try again...

--- src/NeonSiteServiceProvider.php
+++ src/NeonSiteServiceProvider.php
@@ -34,8 +34,8 @@ class NeonSiteServiceProvider extends ServiceProvider
     
     if ($this->app->runningInConsole())
     {
-      dd(Storage::get(__DIR__.'/../config/config.php'));//-, Str::of(Storage::get(__DIR__.'/../config/config.php'))->replace('##uuid##', Str::uuid()));
-      Storage::put(__DIR__.'/../config/config.php', Str::of(Storage::get(__DIR__.'/../config/config.php'))->replace('##uuid##', Str::uuid()));
+      file_put_contents(__DIR__.'/../config/config.php', Str::of(file_get_contents(__DIR__.'/../config/config.php'))->replace('##uuid##', Str::uuid()));
+      // Storage::put(__DIR__.'/../config/config.php', Str::of(Storage::get(__DIR__.'/../config/config.php'))->replace('##uuid##', Str::uuid()));
 
       $this->publishes([
         __DIR__.'/../config/config.php'   => config_path('neon-config.php'),
