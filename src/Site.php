f87b49f1ad09a0004be06cf75d1e52e303039dbf
Balazs Ercsey <balazs.ercsey@elementary-interactive.com>
2022-11-14 22:38:47 +0100

    WIP: Clean up methods and cache added.
    #NEON-5

--- src/Site.php
+++ src/Site.php
@@ -2,13 +2,17 @@
 
 namespace Neon\Site;
 
+use Illuminate\Support\Facades\Cache;
 use Neon\Site\Models\Site as SiteModel;
 
 class Site
 {
-  private $primaryKey = 'id';
+  const DRIVER_FILE = 'file';
+  const DRIVER_DATABASE = 'database';
 
-  private $driver = 'file';
+  private $driver = self::DRIVER_FILE;
+
+  private $class = \Neon\Site\Models\Site::class;
 
   private $sites = null;
 
@@ -17,23 +21,57 @@ class Site
   public function __construct()
   {
     /** Getting driver's value to know from where we should take the list of sites.
+     * @var string Identify resource from where sites should be filled up.
      */
-    $this->driver = config('site.driver');
-    $this->sites = ($this->driver === 'file') ? collect(config('site.hosts')) : SiteModel::all();
-    $this->class = config('site.class');
-    $this->primaryKey = (new $this->class)->getKeyName();
+    $this->driver   = config('site.driver');
 
-    $this->sites->each(function($item, $key) {
-      if (!array_key_exists($this->primaryKey, (array) $item))
-      {
-        if (!is_a($item, $this->class))
-        {
-          $item = new $this->class($item);
-        }
+    /** 
+     * @var string The name of the clas what represents site.
+     */
+    $this->class    = config('site.class');
+
+    $this->boot();
+    // $this->sites    = ($this->driver === self::DRIVER_FILE) ? collect(config('site.hosts')) : SiteModel::all();
+    // $this->primaryKey = (new $this->class)->getKeyName();
+
+    // $this->sites->each(function($item, $key) use ($sites) {
+    //   if (!array_key_exists((new $this->class)->getKeyName(), (array) $item))
+    //   {
+    //     if (!is_a($item, $this->class))
+    //     {
+    //       $item = new $this->class($item);
+    //     }
         
-        $item->{$this->primaryKey} = $key;
+    //     $item->{$this->{(new $this->class)->getKeyName()}} = $key;
+    //   }
+
+    //   return $item;
+    // });
+  }
+
+  private function boot()
+  {
+    if (Cache::has('neon-site'))
+    {
+      $this->sites = Cache::get('neon-site');
+    } else {
+      if ($this->driver === self::DRIVER_DATABASE)
+      {
+        $this->sites = $this->class::all();
       }
-    });
+      if ($this->driver === self::DRIVER_FILE)
+      {
+        $this->sites = collect();
+
+        $sites = collect(config('site.hosts'));
+        $sites->each(function($item, $key) {
+          $this->sites->push(new $this->class($item));
+        });
+      }
+
+      Cache::put('neon-site', $this->sites);
+    }
+    
   }
 
   public function find($host)
