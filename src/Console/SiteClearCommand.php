d61a0083989d23c97d4c3a465a10ec85664e4681
Balazs Ercsey <balazs.ercsey@elementary-interactive.com>
2022-12-03 18:51:48 +0100

    #NEON-7
    Empty cache.

---
+++ src/Console/SiteClearCommand.php
@@ -0,0 +1,22 @@
+// src/Console/SiteClearCommand.php
+<?php
+
+namespace Neon\Site\Console;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Cache;
+
+class SiteClearCommand extends Command
+{
+  /**
+   * @var string The command.
+   */
+  protected $signature = 'site:clear';
+
+  protected $description = 'Clear Neon\' site cache.';
+
+  public final function handle()
+  {
+    Cache::forget('neon-site');
+  }
+}
