725b7668116d9d8f3ab25bb412847f42ede95e5f
Balazs Ercsey <balazs.ercsey@elementary-interactive.com>
2022-12-05 23:09:09 +0100

    #NEON-7
    Add site:generate command.

---
+++ src/Console/SiteGenerateSiteIdCommand.php
@@ -0,0 +1,30 @@
+// src/Console/SiteClearCommand.php
+<?php
+
+namespace Neon\Site\Console;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Cache;
+
+class SiteClearCommand extends Command
+{
+  /**
+   * @var string The command.
+   */
+  protected $signature = 'site:generate';
+
+  protected $description = 'Add Neon\' site id to env.';
+
+  public final function handle()
+  {
+    $path = app()->environmentFilePath();
+
+    $escaped = preg_quote('='.env('SITE_ID'), '/');
+
+    file_put_contents($path, preg_replace(
+        "/^SITE_ID{$escaped}/m",
+       "SITE_ID={\Str::uuid()}",
+       file_get_contents($path)
+    ));
+  }
+}
