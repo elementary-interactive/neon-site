dd3119cf496b763c4d89f51b6b0699b9e38708e6
Balazs Ercsey <balazs.ercsey@elementary-interactive.com>
2022-12-09 20:21:36 +0100

    Add more comments and options to th config files.

--- config/config_database.php
+++ config/config_database.php
@@ -2,6 +2,17 @@
 
 return [
 
+  /** IMPORTANT!!!
+   * 
+   * Sites, to make you project fast and easy to run is hardly caching the sites' configuration even
+   * if its given in the config file or in the database. Take care of change this file, and always
+   * run the next command:
+   * ```
+   *   php artisan site:clear
+   * ```
+   * This will prune all sites caches.
+   */
+
   /** 
    * The driver of the sites' config. This controls the default site "driver"
    * will be used on request. By default it's "file" what means, we will use
@@ -21,16 +32,16 @@ return [
    * - The site's ID will be used as primary key value for site related contents.
    * - Possible arguments:
    *    - domains: List of strings. On these sites we'll use the givem config.
+   *    - locale: The language (locale) identifier of the given site.
    *    - default: Boolean. If no sites could be identified by domain, then we'll select by this.
+   * 
+   * To have the SITE_ID value in the .env file, please run:
+   * ```
+   *   php artisan site:create
+   * ```
    */
   'hosts' => [
     env('SITE_ID') => [
-      'domains' => ['example.com'],
-      'locale'  => 'en',
-      'default' => true
-    ],
-    'dev' => [
-      'domains' => []
     ]
   ]
 ];
