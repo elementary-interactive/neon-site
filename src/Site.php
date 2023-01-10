32a049b11645ad31ff88adbe1191e0ec05693ded
Balazs Ercsey <balazs.ercsey@elementary-interactive.com>
2023-01-10 10:50:25 +0100

    Set default value for config.

--- src/Site.php
+++ src/Site.php
@@ -35,7 +35,7 @@ class Site
 
   private function boot()
   {
-    if (Cache::has('neon-site') && config('site.cache'))
+    if (Cache::has('neon-site') && config('site.cache', true))
     {
       $this->sites = Cache::get('neon-site');
     } else {
